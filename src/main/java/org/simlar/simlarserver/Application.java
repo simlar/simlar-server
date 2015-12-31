e7ce243446f3b81c96b9a7ca0e58b3642ed2aa83
Ben Sartor <ben@simlar.org>
2015-12-31 02:12:19 +0100

    [Application] turned into a spring application

--- src/main/java/org/simlar/simlarserver/Application.java
+++ src/main/java/org/simlar/simlarserver/Application.java
@@ -21,7 +21,12 @@
 
 package org.simlar.simlarserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(final String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
     }
 }
