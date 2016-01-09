ee1f26e407998091053bddc0ac116c9404ea32d2
Ben Sartor <ben@simlar.org>
2016-01-09 21:17:29 +0100

    [Application] made ready for war deployment

--- src/main/java/org/simlar/simlarserver/Application.java
+++ src/main/java/org/simlar/simlarserver/Application.java
@@ -23,10 +23,18 @@ package org.simlar.simlarserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SuppressWarnings("NonFinalUtilityClass")
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(final String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
