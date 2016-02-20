d164aa75224e9721f76a5b5fe7ce4237a9a413e4
Ben Sartor <ben@simlar.org>
2016-02-20 19:46:58 +0100

    [SettingsService] provides properties

---
+++ src/main/java/org/simlar/simlarserver/services/settingsservice/SettingsService.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2015 The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package org.simlar.simlarserver.services.settingsservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Component
+public class SettingsService implements ApplicationListener<ContextRefreshedEvent> {
+    private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
+
+    @Value("${domain:}")
+    private String domain;
+
+
+    public String getDomain() {
+        return domain;
+    }
+
+    @Override
+    public void onApplicationEvent(final ContextRefreshedEvent event) {
+        LOGGER.info("started on domain: " + domain);
+    }
+}
