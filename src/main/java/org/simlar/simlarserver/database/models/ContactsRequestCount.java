20d2e83c1f21d4175e932ad4a66d6862ca0b82fe
Ben Sartor <ben@simlar.org>
2016-03-22 23:24:21 +0100

    [ContactsRequestCount] add new db entity

---
+++ src/main/java/org/simlar/simlarserver/database/models/ContactsRequestCount.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2016 The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package org.simlar.simlarserver.database.models;
+
+import org.simlar.simlarserver.utils.SimlarId;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "simlar_contacts_request_count")
+public final class ContactsRequestCount {
+    @SuppressWarnings("FieldCanBeLocal")
+    @Id
+    @Column(nullable = false, length = 64)
+    private String simlarId;
+
+    @Column(nullable = false)
+    private Timestamp timestamp;
+
+    @Column(nullable = false, length = 64)
+    private String hash;
+
+    @Column(nullable = false, columnDefinition = "int(10) unsigned")
+    private int count;
+
+    @SuppressWarnings("unused")
+    protected ContactsRequestCount() {
+        // no-args constructor required by JPA spec
+        // this one is protected since it shouldn't be used directly
+    }
+
+    public ContactsRequestCount(final SimlarId simlarId, final Timestamp timestamp, final String hash, final int count) {
+        this.simlarId = simlarId.get();
+        this.timestamp = timestamp;
+        this.hash = hash;
+        this.count = count;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
