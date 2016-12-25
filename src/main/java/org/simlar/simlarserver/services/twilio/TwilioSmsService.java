5514a947341b64cdcc67ccde491329a1d6ca92a8
Ben Sartor <ben@simlar.org>
2016-12-25 01:51:53 +0100

    [TwilioSmsService] parse json answers

--- src/main/java/org/simlar/simlarserver/services/twilio/TwilioSmsService.java
+++ src/main/java/org/simlar/simlarserver/services/twilio/TwilioSmsService.java
@@ -21,15 +21,24 @@
 
 package org.simlar.simlarserver.services.twilio;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.simlar.simlarserver.json.twilio.MessageResponse;
 import org.simlar.simlarserver.services.settingsservice.SettingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -46,13 +55,7 @@ public final class TwilioSmsService {
         this.twilioSettingsService = twilioSettingsService;
     }
 
-    @SuppressWarnings({"BooleanMethodNameMustStartWithQuestion", "MethodWithMultipleReturnPoints"})
-    public boolean sendSms(final String telephoneNumber, final String text) {
-        if (!twilioSettingsService.isConfigured()) {
-            LOGGER.severe("twilio not configured: " + twilioSettingsService);
-            return false;
-        }
-
+    private String postRequest(final String telephoneNumber, final String text) {
         final MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
         parameters.add("To", telephoneNumber);
         parameters.add("From", twilioSettingsService.getSmsSourceNumber());
@@ -66,12 +69,50 @@ public final class TwilioSmsService {
                     .postForObject(twilioSettingsService.getUrl(), parameters, String.class);
 
             LOGGER.info("response: " + response);
-            return true;
+            return response;
         } catch (final HttpClientErrorException e) {
-            LOGGER.info("while sending sms to " + telephoneNumber + " received error response: " + e.getMessage() + " response: " + e.getResponseBodyAsString());
-            return false;
+            LOGGER.info("while sending sms to " + telephoneNumber + " received error: " + e.getMessage() + " response: " + e.getResponseBodyAsString());
+            return e.getStatusCode() == HttpStatus.BAD_REQUEST ? e.getResponseBodyAsString() : null;
         } catch (final RestClientException e) {
-            LOGGER.log(Level.SEVERE, "while sending sms to " + telephoneNumber + " failed to connect to twilio server", e);
+            final String cause = ExceptionUtils.getRootCauseMessage(e);
+            LOGGER.log(Level.SEVERE, "while sending sms to " + telephoneNumber + " failed to connect to twilio server: " + cause);
+            return null;
+        }
+    }
+
+    @SuppressWarnings({"BooleanMethodNameMustStartWithQuestion", "MethodWithMultipleReturnPoints"})
+    public boolean sendSms(final String telephoneNumber, final String text) {
+        if (!twilioSettingsService.isConfigured()) {
+            LOGGER.severe("twilio not configured: " + twilioSettingsService);
+            return false;
+        }
+
+        final String response = postRequest(telephoneNumber, text);
+
+        if (StringUtils.isEmpty(response)) {
+            LOGGER.severe("while sending sms to " + telephoneNumber + " received empty response");
+            return false;
+        }
+
+        try {
+            final MessageResponse messageResponse = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).readValue(response, MessageResponse.class);
+            if (StringUtils.isEmpty(messageResponse.getSid())) {
+                LOGGER.severe("while sending sms to " + telephoneNumber + " received message response without MessageSid: " + response);
+                return false;
+            }
+
+            if (StringUtils.isEmpty(messageResponse.getStatus())) {
+                LOGGER.severe("while sending sms to " + telephoneNumber + " received message response without status: " + response);
+                return false;
+            }
+
+            LOGGER.info("while sending sms to " + telephoneNumber + " received message response: " + messageResponse);
+            return true;
+        } catch (final JsonMappingException | JsonParseException e) {
+            LOGGER.log(Level.SEVERE, "while sending sms to " + telephoneNumber + " unable to parse response: " + response, e);
+            return false;
+        } catch (final IOException e) {
+            LOGGER.log(Level.SEVERE, "while sending sms to " + telephoneNumber + " IOException during response parsing: " + response, e);
             return false;
         }
     }
