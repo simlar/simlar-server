d260401061cad75614983678bbcf5666caf48711
Ben Sartor <ben@simlar.org>
2016-12-25 02:40:08 +0100

    [MessageResponse] new class representing twilio's json response message

---
+++ src/main/java/org/simlar/simlarserver/json/twilio/MessageResponse.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2016 The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package org.simlar.simlarserver.json.twilio;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@SuppressWarnings("unused")
+public final class MessageResponse {
+    private String sid;
+    private String to;
+    private String status;
+    private String price;
+    @JsonProperty("priceUnit")
+    private String priceUnit;
+    @JsonProperty("errorCode")
+    private String errorCode;
+    @JsonProperty("message")
+    private String errorMessage;
+
+    // We are not interested in every field of twilio's response. At least the following are ignored:
+    //  account_sid, from, date_created, date_updated, date_sent, messaging_service_sid, body, num_segments, num_media, direction, api_version, uri, subresource_uris, media
+
+    @Override
+    public String toString() {
+        return "MessageResponse{" +
+                "sid='" + sid + '\'' +
+                ", to='" + to + '\'' +
+                ", status='" + status + '\'' +
+                ", price='" + price + '\'' +
+                ", priceUnit='" + priceUnit + '\'' +
+                ", errorCode='" + errorCode + '\'' +
+                ", errorMessage='" + errorMessage + '\'' +
+                '}';
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
