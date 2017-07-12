a3472df58a59662f6b871eb1db5d45ed7f930e2c
Ben Sartor <ben@simlar.org>
2017-07-12 05:36:00 +0200

    [XmlSuccessCreateAccountRequestTest] marshal

---
+++ src/test/java/org/simlar/simlarserver/xml/XmlSuccessCreateAccountRequestTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2017 The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package org.simlar.simlarserver.xml;
+
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
+
+public final class XmlSuccessCreateAccountRequestTest {
+    @Test
+    public void testMarshal() throws JAXBException {
+        final XmlSuccessCreateAccountRequest response = new XmlSuccessCreateAccountRequest("*12345*", "s1cur3Me");
+        final StringWriter writer = new StringWriter();
+        JAXBContext.newInstance(XmlSuccessCreateAccountRequest.class).createMarshaller().marshal(response, writer);
+        final String xml = writer.toString();
+        assertEquals(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><success password=\"s1cur3Me\" simlarId=\"*12345*\"/>",
+                xml);
+    }
+}
