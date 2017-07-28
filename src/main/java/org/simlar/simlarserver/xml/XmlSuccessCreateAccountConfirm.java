df7c0d6400d842494ef4badf043a2639103fa00a
Ben Sartor <ben@simlar.org>
2017-07-28 03:06:20 +0200

    [XmlSuccessCreateAccountConfirm] new xml response

---
+++ src/main/java/org/simlar/simlarserver/xml/XmlSuccessCreateAccountConfirm.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2016 The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package org.simlar.simlarserver.xml;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "success")
+public final class XmlSuccessCreateAccountConfirm {
+    private String simlarId;
+    private String registrationCode;
+
+    public XmlSuccessCreateAccountConfirm() {
+        // needed for JAXBContext
+    }
+
+    public XmlSuccessCreateAccountConfirm(final String simlarId, final String password) {
+        this.simlarId = simlarId;
+        this.registrationCode = password;
+    }
+
+
+    @XmlAttribute
+    public String getSimlarId() {
+        return simlarId;
+    }
+
+    @SuppressFBWarnings("UPM_UNCALLED_PRIVATE_METHOD")
+    @SuppressWarnings("unused")
+    private void setSimlarId(final String simlarId) {
+        this.simlarId = simlarId;
+    }
+
+
+    @XmlAttribute
+    public String getRegistrationCode() {
+        return registrationCode;
+    }
+
+    @SuppressFBWarnings("UPM_UNCALLED_PRIVATE_METHOD")
+    @SuppressWarnings("unused")
+    private void setRegistrationCode(final String password) {
+        this.registrationCode = password;
+    }
+}
