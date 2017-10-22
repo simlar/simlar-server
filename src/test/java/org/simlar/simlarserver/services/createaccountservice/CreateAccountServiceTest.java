19382215a9e8eda03098ebe0a0fd34ae4aca0c79
Ben Sartor <ben@simlar.org>
2017-10-22 18:12:38 +0200

    [CreateAccountServiceTest] test CreateAccountRequest telephone number limit

--- src/test/java/org/simlar/simlarserver/services/createaccountservice/CreateAccountServiceTest.java
+++ src/test/java/org/simlar/simlarserver/services/createaccountservice/CreateAccountServiceTest.java
@@ -27,17 +27,23 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.simlar.simlarserver.Application;
+import org.simlar.simlarserver.services.settingsservice.SettingsService;
 import org.simlar.simlarserver.services.smsservice.SmsService;
 import org.simlar.simlarserver.xmlerrorexceptions.XmlErrorFailedToSendSmsException;
 import org.simlar.simlarserver.xmlerrorexceptions.XmlErrorInvalidTelephoneNumberException;
+import org.simlar.simlarserver.xmlerrorexceptions.XmlErrorTooManyRequestTriesException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Objects;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -49,6 +55,9 @@ public final class CreateAccountServiceTest {
 
     @Autowired
     private SmsService smsService;
+
+    @Autowired
+    private SettingsService settingsService;
 
     @Rule
     public final ExpectedException expectedException = ExpectedException.none();
@@ -85,5 +94,37 @@ public final class CreateAccountServiceTest {
         when(smsService.sendSms(eq(telephoneNumber), anyString())).thenReturn(Boolean.TRUE);
         createAccountService.createAccountRequest(telephoneNumber, "", "");
         verify(smsService).sendSms(eq(telephoneNumber), anyString());
+    }
+
+    private static void assertException(final Class<? extends Exception> expected, final Runnable runnable) {
+        //noinspection OverlyBroadCatchBlock
+        try {
+            runnable.run();
+            fail("expected exception not thrown: " + expected.getSimpleName());
+        } catch (final Exception exception) {
+            if (!Objects.equals(expected, exception.getClass())) {
+                throw new AssertionError("expected exception '" + expected.getSimpleName() + "' but '" + exception.getClass().getSimpleName() + "' was thrown", exception);
+            }
+        }
+    }
+
+    @Test
+    public void testCreateAccountRequestTelephoneNumberLimit() {
+        final String telephoneNumber = "+15005023024";
+
+        final int max = settingsService.getAccountCreationMaxRequestsPerSimlarIdPerDay();
+        for (int i = 0; i < max; i++) {
+            reset(smsService);
+            if ((i & 1) == 0) {
+                assertException(XmlErrorFailedToSendSmsException.class, () -> createAccountService.createAccountRequest(telephoneNumber, "", ""));
+            } else {
+                when(smsService.sendSms(eq(telephoneNumber), anyString())).thenReturn(Boolean.TRUE);
+                createAccountService.createAccountRequest(telephoneNumber, "", "");
+                verify(smsService).sendSms(eq(telephoneNumber), anyString());
+            }
+        }
+
+        expectedException.expect(XmlErrorTooManyRequestTriesException.class);
+        createAccountService.createAccountRequest(telephoneNumber, "", "");
     }
 }
