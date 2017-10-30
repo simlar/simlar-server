bbc62792aa9b8ce2d471d9e664c3fd90bc55831e
Ben Sartor <ben@simlar.org>
2017-10-31 00:51:23 +0100

    [TwilioSmsService] fix delivery report url

--- src/main/java/org/simlar/simlarserver/services/twilio/TwilioSmsService.java
+++ src/main/java/org/simlar/simlarserver/services/twilio/TwilioSmsService.java
@@ -63,7 +63,7 @@ public final class TwilioSmsService implements SmsService {
         parameters.add("From", twilioSettingsService.getSmsSourceNumber());
         parameters.add("StatusCallback", "https://" +
                 twilioSettingsService.getCallbackUser() + ':' + twilioSettingsService.getCallbackPassword() + '@' +
-                settingsService.getDomain() + ":6161/twilio/delivery-report.php");
+                settingsService.getDomain() + ":6161/twilio/delivery-report.json");
         parameters.add("Body", text);
 
         try {
