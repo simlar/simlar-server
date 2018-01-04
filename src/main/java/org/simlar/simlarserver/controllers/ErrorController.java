7fe926da541051509c2f2b68ea730932191c7264
Ben Sartor <ben@simlar.org>
2018-01-04 23:07:36 +0100

    [RequestLogMessage] new class for logging HttpServletRequests

--- src/main/java/org/simlar/simlarserver/controllers/ErrorController.java
+++ src/main/java/org/simlar/simlarserver/controllers/ErrorController.java
@@ -22,6 +22,7 @@
 package org.simlar.simlarserver.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.simlar.simlarserver.utils.RequestLogMessage;
 import org.simlar.simlarserver.xml.XmlError;
 import org.simlar.simlarserver.xmlerrorexceptionclientresponse.XmlErrorExceptionClientResponse;
 import org.simlar.simlarserver.xmlerrorexceptions.XmlErrorException;
@@ -33,29 +34,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import java.io.StringWriter;
-import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
 @RestController
 final class ErrorController {
-    private static String createLog(final HttpServletRequest request) {
-        return request == null
-                ? "no request object"
-                : "URL='" + request.getRequestURL() + "' IP='" + request.getRemoteAddr() + "' User-Agent='" + request.getHeader("User-Agent") + "' parameters='" + serializeParameters(request) + '\'';
-    }
-
-    private static String serializeParameters(final ServletRequest request) {
-        return request.getParameterMap().entrySet().stream()
-                .map(e -> e.getKey() + "=\"" + String.join(" ", e.getValue()) + '"')
-                .collect(Collectors.joining(", "));
-    }
-
     private static XmlError createXmlError(final XmlErrorExceptionClientResponse response) {
         return new XmlError(response.getId(), response.getMessage());
     }
@@ -76,7 +63,7 @@ final class ErrorController {
     @RequestMapping(path = "*")
     // in order to handle html request errors we have to return a String here
     public static String handle(final HttpServletRequest request) {
-        log.warn("Request Error with request='{}'", createLog(request));
+        log.warn("Request Error with request='{}'", new RequestLogMessage(request));
         return createXmlErrorString(XmlErrorExceptionClientResponse.UNKNOWN_STRUCTURE);
     }
 
@@ -85,18 +72,18 @@ final class ErrorController {
         final Class<? extends XmlErrorException> exceptionClass = xmlErrorException.getClass();
         final XmlErrorExceptionClientResponse response = XmlErrorExceptionClientResponse.fromException(exceptionClass);
         if (response == null) {
-            log.error("XmlErrorException with no XmlErrorExceptionClientResponse found for '{}' with request='{}'", exceptionClass.getSimpleName(), createLog(request), xmlErrorException);
+            log.error("XmlErrorException with no XmlErrorExceptionClientResponse found for '{}' with request='{}'", exceptionClass.getSimpleName(), new RequestLogMessage(request), xmlErrorException);
             return createXmlError(XmlErrorExceptionClientResponse.UNKNOWN_ERROR);
         }
 
-        log.warn("'{}' => XmlError('{}', '{}') {} with request='{}'", xmlErrorException.getClass().getSimpleName(), response.getId(), response.getMessage(), xmlErrorException.getMessage(), createLog(request));
+        log.warn("'{}' => XmlError('{}', '{}') {} with request='{}'", xmlErrorException.getClass().getSimpleName(), response.getId(), response.getMessage(), xmlErrorException.getMessage(), new RequestLogMessage(request));
 
         return createXmlError(response);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public static String handleMissingParameterException(final HttpServletRequest request, final MissingServletRequestParameterException exception) {
-        log.error("{}: {} with request='{}'", exception.getClass().getSimpleName(), exception.getMessage(), createLog(request), exception);
+        log.error("{}: {} with request='{}'", exception.getClass().getSimpleName(), exception.getMessage(), new RequestLogMessage(request), exception);
 
         return createXmlErrorString(XmlErrorExceptionClientResponse.UNKNOWN_STRUCTURE);
     }
@@ -104,7 +91,7 @@ final class ErrorController {
     // in order to handle html request errors we have to return a String here
     @ExceptionHandler(Exception.class)
     public static String handleException(final HttpServletRequest request, final Exception exception) {
-        log.error("unhandled '{}' with request='{}'", exception.getClass().getSimpleName(), createLog(request), exception);
+        log.error("unhandled '{}' with request='{}'", exception.getClass().getSimpleName(), new RequestLogMessage(request), exception);
 
         return createXmlErrorString(XmlErrorExceptionClientResponse.UNKNOWN_ERROR);
     }
---
+++ src/main/java/org/simlar/simlarserver/utils/RequestLogMessage.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package org.simlar.simlarserver.utils;
+
+import lombok.AllArgsConstructor;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public final class RequestLogMessage {
+    private final HttpServletRequest request;
+
+    @Override
+    public String toString() {
+        return request == null
+                ? "no request object"
+                : "URL='" + request.getRequestURL() + "' IP='" + request.getRemoteAddr() + "' User-Agent='" + request.getHeader("User-Agent") + "' parameters='" + serializeParameters(request) + '\'';
+    }
+
+    private static String serializeParameters(final ServletRequest request) {
+        return request.getParameterMap().entrySet().stream()
+                .map(e -> e.getKey() + "=\"" + String.join(" ", e.getValue()) + '"')
+                .collect(Collectors.joining(", "));
+    }
+}
