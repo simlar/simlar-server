5de63f17f0367baff9755fadb24ef39ee3b0a0ca
Ben Sartor <ben@simlar.org>
2018-01-09 10:42:44 +0100

    [XmlSuccessPushNotification] set property order

--- src/main/java/org/simlar/simlarserver/xml/XmlSuccessPushNotification.java
+++ src/main/java/org/simlar/simlarserver/xml/XmlSuccessPushNotification.java
@@ -29,12 +29,14 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "success")
+@XmlType(propOrder = { "deviceType", "pushId" })
 public final class XmlSuccessPushNotification {
     @XmlAttribute
     private int deviceType;
