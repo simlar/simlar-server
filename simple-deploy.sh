b82fd3f61d6552b8de038dfd7ce71dad400cfa4e
Ben Sartor <ben@simlar.org>
2018-01-14 15:28:04 +0100

    Revert "[simple-deploy.sh] restart tomcat"
    This reverts commit 616dd5acaef5fed25272ff3a8b66028e7a6a55b9.

--- simple-deploy.sh
+++ simple-deploy.sh
@@ -5,7 +5,5 @@ declare -r  SERVER=${1:?"USAGE: $0 your.server.org"}
 
 scp build/libs/simlar-server*.war root@"${SERVER}":/tmp/
 ssh root@"${SERVER}" "rm /var/lib/tomcat8/webapps/simlar-server*.war ; mv /tmp/simlar-server*.war /var/lib/tomcat8/webapps/"
-ssh root@"${SERVER}" "systemctl stop tomcat8.service"
-ssh root@"${SERVER}" "systemctl start tomcat8.service"
 sleep 2s
 ssh root@"${SERVER}" "curl http://127.0.0.1:8080/simlar-server/version"
