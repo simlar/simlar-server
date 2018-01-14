76c5c3a6be56335f36aa61bf44d702e907546569
Ben Sartor <ben@simlar.org>
2018-01-14 15:26:06 +0100

    [simple-deploy.sh] check deployed version

--- simple-deploy.sh
+++ simple-deploy.sh
@@ -5,3 +5,5 @@ declare -r  SERVER=${1:?"USAGE: $0 your.server.org"}
 
 scp build/libs/simlar-server*.war root@"${SERVER}":/tmp/
 ssh root@"${SERVER}" "rm /var/lib/tomcat8/webapps/simlar-server*.war ; mv /tmp/simlar-server*.war /var/lib/tomcat8/webapps/"
+sleep 2s
+ssh root@"${SERVER}" "curl http://127.0.0.1:8080/simlar-server/version"
