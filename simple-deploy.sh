a29f21a392e523ffe6d2c9545e158074db6720ad
Ben Sartor <ben@simlar.org>
2018-01-14 15:00:45 +0100

    [simple-deploy.sh] first copy and than install war file

--- simple-deploy.sh
+++ simple-deploy.sh
@@ -3,4 +3,5 @@ set -eu -o pipefail
 
 declare -r  SERVER=${1:?"USAGE: $0 your.server.org"}
 
-scp build/libs/simlar-server-*.war root@"${SERVER}":/var/lib/tomcat8/webapps/simlar-server.war
+scp build/libs/simlar-server*.war root@"${SERVER}":/tmp/
+ssh root@"${SERVER}" "rm /var/lib/tomcat8/webapps/simlar-server*.war ; mv /tmp/simlar-server*.war /var/lib/tomcat8/webapps/"
