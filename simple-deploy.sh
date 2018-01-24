18e8d72bd43b69fa4b1c87b8f1e77a6d3cd0653d
Ben Sartor <ben@simlar.org>
2018-01-24 23:18:01 +0100

    [simple-deploy.sh] check old version

--- simple-deploy.sh
+++ simple-deploy.sh
@@ -15,18 +15,25 @@ function cleanup {
 trap cleanup EXIT
 
 
+declare -r VERSION_OLD=$(ssh ${REMOTE} "curl --silent http://127.0.0.1:8080/simlar-server/version")
+echo "current version on server: ${VERSION_OLD}"
+
+
 echo "build war file with version: $(git describe --tags --always)"
 ./gradlew clean war
+
 
 echo -e "\n\n"
 echo "copy war file"
 scp build/libs/simlar-server*.war ${REMOTE}:"${REMOTE_DIR}/"
 
+
 echo "install war file"
 ssh ${REMOTE} "rm /var/lib/tomcat8/webapps/simlar-server*.war ; mv "${REMOTE_DIR}"/simlar-server*.war /var/lib/tomcat8/webapps/"
 
-echo "wait"
-sleep 15s
 
+echo "waiting"
+sleep 15s
 echo "check version"
-ssh ${REMOTE} "curl --silent http://127.0.0.1:8080/simlar-server/version"
+declare -r VERSION_NEW=$(ssh ${REMOTE} "curl --silent http://127.0.0.1:8080/simlar-server/version")
+echo "update success: ${VERSION_OLD} -> ${VERSION_NEW}"
