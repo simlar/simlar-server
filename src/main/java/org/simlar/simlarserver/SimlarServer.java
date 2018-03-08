2913ed7ada8890dd1490580267efdaef80141963
Ben Sartor <ben@simlar.org>
2018-03-08 23:09:23 +0100

    [SimlarServer] spring boot 2 update

--- src/main/java/org/simlar/simlarserver/SimlarServer.java
+++ src/main/java/org/simlar/simlarserver/SimlarServer.java
@@ -24,7 +24,7 @@ package org.simlar.simlarserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource(value = "file:/etc/simlar-server/config.properties", ignoreResourceNotFound = true)
