c36766af948625ffdcc241557d881da3af3c7b22
Ben Sartor <ben@simlar.org>
2018-03-24 14:48:40 +0100

    [examples/simlar_account_creation_request.sql] add history

--- examples/simlar_account_creation_request.sql
+++ examples/simlar_account_creation_request.sql
@@ -10,4 +10,23 @@ CREATE TABLE IF NOT EXISTS simlar_account_creation_request (
     PRIMARY KEY (`simlarId`),
     KEY `request_timestamp` (`timestamp`),
     KEY `request_ip` (`ip`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- History
+--
+
+CREATE TABLE IF NOT EXISTS simlar_account_creation_request (
+    `simlarId` varchar(64) NOT NULL,
+    `password` varchar(64) NOT NULL,
+    `registrationCode` varchar(64) NOT NULL,
+    `requestTries` int(10) unsigned NOT NULL DEFAULT 1,
+    `confirmTries` int(10) unsigned NOT NULL DEFAULT 0,
+    `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `ip` varchar(64) NOT NULL,
+    PRIMARY KEY (`simlarId`),
+    KEY `request_timestamp` (`timestamp`),
+    KEY `request_ip` (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `simlar_account_creation_request` ADD COLUMN `calls` int(10) unsigned NOT NULL DEFAULT '0' AFTER `requestTries`;
