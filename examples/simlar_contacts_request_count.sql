03ba8d68955a138555bcb5cb49b81eaa2dcfbd78
Ben Sartor <ben@simlar.org>
2018-04-14 21:48:29 +0200

    [examples] uses spaces instead of tabs

--- examples/simlar_contacts_request_count.sql
+++ examples/simlar_contacts_request_count.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS simlar_contacts_request_count (
-	`simlarId` varchar(64) NOT NULL,
-	`count` int(10) unsigned NOT NULL,
-	`hash` varchar(64) NOT NULL,
-	`timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (`simlarId`)
+    `simlarId` varchar(64) NOT NULL,
+    `count` int(10) unsigned NOT NULL,
+    `hash` varchar(64) NOT NULL,
+    `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`simlarId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
