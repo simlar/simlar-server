dc30404c2ca23df9eb49790e22c4340450cb3b39
Ben Sartor <ben@simlar.org>
2019-03-26 23:11:34 +0100

    [RegionalSettings] add constructor binding todo

--- src/main/java/org/simlar/simlarserver/services/createaccountservice/RegionalSettings.java
+++ src/main/java/org/simlar/simlarserver/services/createaccountservice/RegionalSettings.java
@@ -3,6 +3,9 @@ package org.simlar.simlarserver.services.createaccountservice;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * TODO: Once spring boot 2.2 releases, use constructor binding and make this class immutable.
+ */
 @Setter
 @Getter
 final class RegionalSettings {
