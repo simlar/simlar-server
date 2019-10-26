5b87cad81bea6dc20d856b6c28065410b1a6562d
Ben Sartor <ben@simlar.org>
2019-10-26 17:36:07 +0200

    Revert "[ApplePushNotificationMockServerTest] use @Rule"
    This reverts commit 71fc9afc5b011c1159f30f3a7c95c9fb82d77f64.

--- src/test/java/org/simlar/simlarserver/services/pushnotification/ApplePushNotificationMockServerTest.java
+++ src/test/java/org/simlar/simlarserver/services/pushnotification/ApplePushNotificationMockServerTest.java
@@ -1,30 +1,25 @@
 package org.simlar.simlarserver.services.pushnotification;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
+import org.mockserver.client.MockServerClient;
 import org.mockserver.integration.ClientAndServer;
-import org.mockserver.junit.MockServerRule;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
 @SuppressFBWarnings("UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR")
-@SuppressWarnings("resource")
+@SuppressWarnings({"IOResourceOpenedButNotSafelyClosed", "resource"}) // MockServerClient used as documented
 public final class ApplePushNotificationMockServerTest {
     private ApplePushNotification applePushNotification;
 
-    @SuppressWarnings("ThisEscapedInObjectConstruction") // used as documented
-    @Rule
-    public final MockServerRule mockServerRule = new MockServerRule(this);
-
-    @SuppressFBWarnings({"UUF_UNUSED_FIELD", "FCBL_FIELD_COULD_BE_LOCAL"})
-    @SuppressWarnings("unused")
     private ClientAndServer mockServer;
 
     @Before
@@ -34,13 +29,15 @@ public final class ApplePushNotificationMockServerTest {
                 .appleVoipCertificatePath("src/test/resources/self-signed.p12")
                 .appleVoipCertificatePassword("s3cr3t")
                 .build());
+
+        mockServer = startClientAndServer();
     }
 
     @SuppressFBWarnings("UTAO_JUNIT_ASSERTION_ODDITIES_NO_ASSERT")
     @SuppressWarnings({"JUnitTestMethodWithNoAssertions", "PMD.JUnitTestsShouldIncludeAssert"})
     @Test
     public void testRequestAppleVoipPushNotification() {
-        mockServerRule.getClient()
+        new MockServerClient("localhost", mockServer.getLocalPort())
                 .when(
                         request()
                                 .withMethod("POST")
@@ -54,12 +51,12 @@ public final class ApplePushNotificationMockServerTest {
                                 .withStatusCode(200)
                 );
 
-        applePushNotification.requestVoipPushNotification("http://localhost:" + mockServerRule.getPort() + "/deviceToken", "localhost");
+        applePushNotification.requestVoipPushNotification("http://localhost:" + mockServer.getLocalPort() + "/deviceToken", "localhost");
     }
 
     @Test
     public void testRequestAppleVoipPushNotificationWithInvalidDeviceToken() {
-        mockServerRule.getClient()
+        new MockServerClient("localhost", mockServer.getLocalPort())
                 .when(
                         request()
                                 .withMethod("POST")
@@ -72,11 +69,16 @@ public final class ApplePushNotificationMockServerTest {
                 );
 
         try {
-            applePushNotification.requestVoipPushNotification("http://localhost:" + mockServerRule.getPort() + "/invalidDeviceToken", "localhost");
+            applePushNotification.requestVoipPushNotification("http://localhost:" + mockServer.getLocalPort() + "/invalidDeviceToken", "localhost");
             fail("expected exception not thrown: " + HttpClientErrorException.class.getSimpleName());
         } catch (final HttpClientErrorException e) {
             assertEquals(HttpStatus.BAD_REQUEST, e.getStatusCode());
             assertEquals("{\"reason\":\"BadDeviceToken\"}", e.getResponseBodyAsString());
         }
+    }
+
+    @After
+    public void after() {
+        mockServer.stop();
     }
 }
