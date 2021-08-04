e94a798ac71495f3a32c6d38cfd1d10e652ef80b
Ben Sartor <ben@simlar.org>
2021-08-04 23:15:42 +0200

    [TestAccount] settings for test accounts

---
+++ src/main/java/org/simlar/simlarserver/services/createaccountservice/TestAccount.java
@@ -0,0 +1,15 @@
+package org.simlar.simlarserver.services.createaccountservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@AllArgsConstructor
+@Getter
+@ConstructorBinding
+@ConfigurationProperties
+final class TestAccount {
+    private final String simlarId;
+    private final String registrationCode;
+}
