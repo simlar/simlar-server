4c8829d23d516cbb7494d380b73114aa5a633e89
Ben Sartor <ben@simlar.org>
2022-04-01 20:33:21 +0200

    [BinderControllerAdvice] new controller advice to mitigate spring for shell

---
+++ src/main/java/org/simlar/simlarserver/controllers/BinderControllerAdvice.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package org.simlar.simlarserver.controllers;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+@Order(10000)
+final class BinderControllerAdvice {
+    @SuppressWarnings("MethodMayBeStatic")
+    @InitBinder
+    public void setAllowedFields(final WebDataBinder dataBinder) {
+        // spring4shell mitigation  see: https://www.lunasec.io/docs/blog/spring-rce-vulnerabilities/
+        dataBinder.setDisallowedFields("class.*", "Class.*", "*.class.*", "*.Class.*");
+    }
+}
