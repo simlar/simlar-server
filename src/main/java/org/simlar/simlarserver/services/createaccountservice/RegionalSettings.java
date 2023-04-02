7f2347fff5968cd24e3e927b3eb01c3d0c0a6b8b
Ben Sartor <ben@simlar.org>
2023-04-02 14:41:24 +0200

    [RegionalSettings] implement as record

--- src/main/java/org/simlar/simlarserver/services/createaccountservice/RegionalSettings.java
+++ src/main/java/org/simlar/simlarserver/services/createaccountservice/RegionalSettings.java
@@ -1,14 +1,10 @@
 package org.simlar.simlarserver.services.createaccountservice;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SuppressWarnings("unused")
-@AllArgsConstructor
-@Getter
 @ConfigurationProperties
-final class RegionalSettings {
-    private final String regionCode;
-    private final int maxRequestsPerHour;
+public record RegionalSettings(
+        String regionCode,
+        int maxRequestsPerHour) {
 }
