4c264182d8627ed79f6a5a26a3810a04a436e413
Ben Sartor <ben@simlar.org>
2024-09-25 20:42:33 +0200

    [DeleteAccountController] add webcontroller with request page

---
+++ src/main/java/org/simlar/simlarserver/webcontrollers/deleteaccountcontroller/DeleteAccountController.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package org.simlar.simlarserver.webcontrollers.deleteaccountcontroller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SuppressWarnings({"SameReturnValue", "ClassWithTooManyTransitiveDependencies"})
+@AllArgsConstructor
+@Slf4j
+@Controller
+final class DeleteAccountController {
+    public static final String REQUEST_PATH_REQUEST = "/delete-account/request";
+
+    @GetMapping(REQUEST_PATH_REQUEST)
+    public static String requestForm(final Model model) {
+        return "delete-account/request";
+    }
+}
