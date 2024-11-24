6c1cade96de39450c5a0ef23bc397a828b68fcd4
Ben Sartor <ben@simlar.org>
2024-11-25 00:55:11 +0100

    [DeleteAccountControllerTest] fix deprecated @MockBean

--- src/test/java/org/simlar/simlarserver/webcontrollers/deleteaccountcontroller/DeleteAccountControllerTest.java
+++ src/test/java/org/simlar/simlarserver/webcontrollers/deleteaccountcontroller/DeleteAccountControllerTest.java
@@ -8,8 +8,8 @@ import org.simlar.simlarserver.xmlerrorexceptions.XmlErrorFailedToSendSmsExcepti
 import org.simlar.simlarserver.xmlerrorexceptions.XmlErrorWrongRegistrationCodeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,7 +33,7 @@ public final class DeleteAccountControllerTest {
     private MockMvc mockMvc;
 
     @SuppressWarnings("unused")
-    @MockBean
+    @MockitoBean
     private AccountService accountService;
 
     @SuppressWarnings({"TestMethodWithoutAssertion", "PMD.JUnitTestsShouldIncludeAssert"})
